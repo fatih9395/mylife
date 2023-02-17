6bdf5f209090727d835de09895737cbd073fe7e5
fatih9395 <m.fatihdegirmenci18@gmail.com>
2023-02-16 22:21:29 -0500

    Created views and routes for family tree

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,31 @@ router.get('/', (req, res,)=> {
 });
 
 router.get('/about',(req, res)=>{
-  res.render('about');
+  //Displaying current date
+  let date = new Date();
+  res.render('about', {
+    date : date, 
+    //Adding a title for our page
+    title : 'Purpose of the Site'
+  });
+});
+
+router.get('/father',(req, res)=>{
+  res.render('father',{
+    title : 'Father'
+  });
+});
+
+router.get('/mother',(req, res)=>{
+  res.render('mother',{
+    title : 'Mother'
+  });
+});
+
+router.get('/brother',(req, res)=>{
+  res.render('brother',{
+    title : 'Brother'
+  });
 });
 
 module.exports = router;
